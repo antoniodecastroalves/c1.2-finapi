929d958191011cf92a2c7767279412643935185f
Antonio Alves <antoniodecastroalves@gmail.com>
2021-05-01 16:40:43 -0300

    validating the account

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.post("/account", (request, response) => {
   );
 
   if (customerAlreadyExists) {
-    return response.status(400).json({error: "Customer already Exists!"});
+    return response.status(400).json({ error: "Customer already Exists!" });
   }
 
   customers.push({
@@ -28,10 +28,14 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement/:cpf", (request, response) => {
-  const { cpf } = request.params;
+app.get("/statement", (request, response) => {
+  const { cpf } = request.headers;
 
   const customer = customers.find(customer => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.json({ error: "Customer not found" });
+  }
 
   return response.json(customer.statement);
 });
